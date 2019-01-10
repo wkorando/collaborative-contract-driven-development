6d008a4888c633cf37e96aa4f97b417ec1561e9a
Billy Korando <wkorando@gmail.com>
2019-01-09 22:27:28 -0500

    Updates for demos

--- contracts/add-movie.groovy
+++ contracts/add-movie.groovy
@@ -7,7 +7,10 @@ Contract.make {
         method 'POST'
         url '/api/v1/movies'
         body([ 
-    		   
+    		movieName: "Predator",
+    		genre: "Action",
+    		releaseYear: 1989
+    		runtime: 107   
 		])
 		headers { 
 			header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE)
@@ -16,7 +19,7 @@ Contract.make {
     response {
         status 201
 		headers { 
-			header(HttpHeaders.LOCATION, 'footballplays/1'
+			header(HttpHeaders.LOCATION, '/api/v1/movies/1'
 				   HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_UTF8_VALUE)
 		}
     }
